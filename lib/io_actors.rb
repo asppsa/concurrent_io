eac132341f7d42d516b9277a2c92ad689ceda0ef
Alastair Pharo <asppsa@gmail.com>
2015-06-11 00:43:46 +1000

    Use atomic value for default selector

--- lib/io_actors.rb
+++ lib/io_actors.rb
@@ -12,38 +12,29 @@ module IOActors
   EnableWriteMessage = Struct.new(:io)
 
   @default_selector_name = 'default_selector'
-  @default_selector = Concurrent::IVar.new
+  @default_selector = Concurrent::Atomic.new(nil)
 
   class << self
+    def default_selector
+      @default_selector.value || @default_selector.update do |old_selector|
+        old_selector || spawn_selector(@default_selector_name)
+      end
+    end
 
     ##
-    # This is not thread-safe, and in general shouldn't be used.  It
-    # exists to make testing of various selectors easier
+    # Using a block here ensures that the previous default selector
+    # has been stopped before the new one is started
+    def replace_default_selector! &block
+      @default_selector.update do |old_selector|
+        old_selector.ask!(:stop) if
+          old_selector && old_selector.respond_to?(:ask!)
+
+        block.call
+      end
+    end
+
     def reset_default_selector!
-      selector = nil
-      if @default_selector.fulfilled?
-        selector = @default_selector.value
-        selector.ask! :stop
-      end
-      @default_selector = Concurrent::IVar.new
-      selector
-    end
-
-    def default_selector
-      # Only attempt to set when it looks unfulfilled.
-      unless @default_selector.fulfilled?
-        begin
-          @default_selector.
-            set spawn_selector(@default_selector_name)
-        rescue Concurrent::MultipleAssignmentError
-        end
-      end
-
-      @default_selector.value
-    end
-
-    def default_selector= selector
-      @default_selector.set selector
+      replace_default_selector!{ nil }
     end
 
     def spawn_selector name
@@ -53,7 +44,7 @@ module IOActors
     end
 
     def use_ffi_libevent!
-      self.default_selector = spawn_ffi_libevent_selector(@default_selector_name)
+      replace_default_selector!{ spawn_ffi_libevent_selector(@default_selector_name) }
     end
 
     def spawn_ffi_libevent_selector name
@@ -62,7 +53,7 @@ module IOActors
     end
 
     def use_nio4r!
-      self.default_selector = spawn_nio4r_selector(@default_selector_name)
+      replace_default_selector!{ spawn_nio4r_selector(@default_selector_name) }
     end
 
     def spawn_nio4r_selector name
@@ -71,7 +62,7 @@ module IOActors
     end
 
     def use_select!
-      self.default_selector = spawn_select_selector(@default_selector_name)
+      replace_default_selector!{ spawn_select_selector(@default_selector_name) }
     end
 
     def spawn_select_selector name
@@ -109,7 +100,7 @@ if ENV['IOACTORS_DEBUG']
     c.logger = l.method(:add)
   end
 
-  if Kernel.const_defined? 'FFI::Libevent'
+  if Kernel.const_defined?(:FFI) && FFI.const_defined?(:Libevent)
     FFI::Libevent.logger = l
   end
 end
