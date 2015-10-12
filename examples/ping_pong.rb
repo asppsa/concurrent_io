6dccfe0ad611ac01121f0d2ad2cc2d312bc76403
Alastair Pharo <asppsa@gmail.com>
2015-10-12 22:37:08 +1100

    Update ping pong example to use actorless setup

--- examples/ping_pong.rb
+++ examples/ping_pong.rb
@@ -5,12 +5,9 @@ require 'bundler/setup'
 require 'ffi/libevent'
 require 'nio'
 require 'io_actors'
-require 'io_actors/selector/nio4r'
-require 'io_actors/selector/ffi_libevent'
 
-require_relative 'ping_pong_actor.rb'
+require_relative 'ping_ponger.rb'
 require 'concurrent/timer_task'
-require 'concurrent/utilities'
 
 # choose a select implementation
 #IOActors.use_ffi_libevent!
@@ -50,18 +47,28 @@ end
 launch_pairs = proc do
   socket_pairs = num.times.map{ UNIXSocket.pair }
 
-  pingers = num.times.map{ |i| PingPongActor.spawn("pinger_#{v}_#{i}", socket_pairs[i][0]) }
-  pongers = num.times.map{ |i| PingPongActor.spawn("ponger_#{v}_#{i}", socket_pairs[i][1]) }
+  pingers = num.times.map{ |i| PingPonger.new(:pinger, v, i, socket_pairs[i][0]) }
+  pongers = num.times.map{ |i| PingPonger.new(:ponger, v, i, socket_pairs[i][1]) }
 
-  pingers.map do |pinger|
-    pinger << :start
-  end
+  pingers.each(&:start!)
 end
 
 # Every 2 seconds kill and restart everything
 Concurrent::TimerTask.execute(execution_interval: interval, timeout_interval: interval, now: true) do
+  puts %{
+
+
+***********
+* KILLING *
+***********
+
+
+
+}
+
+
   while p = pingers.shift
-    p << :die
+    p.die!
   end
 
   # The pongers should die by themselves
@@ -83,79 +90,33 @@ loop do
     puts("=" * now.length)
     puts
 
+    puts %{
+PINGS: #{ PingPongStats.pings }
+PONGS: #{ PingPongStats.pongs }
+}
+    puts
+
     ObjectSpace.each_object(Concurrent::ThreadPoolExecutor) do |exec|
       puts "QUEUE LENGTH: #{exec.queue_length} / #{exec.max_queue}"
       puts "POOL SIZE: #{exec.length}"
     end
 
-    stats = {:terminated => {},
-             :alive => {}}
+    stats = {:failed => 0,
+             :alive => 0}
 
-    ObjectSpace.each_object(Concurrent::Actor::Reference) do |a|
-      k = if a.ask!(:terminated?)
-            :terminated
+    ObjectSpace.each_object(Concurrent::Agent) do |a|
+      k = if a.failed?
+            :failed
           else
             :alive
           end
 
-      cl = if a.actor_class == IOActors::Controller
-             :controller
-           elsif a.actor_class == IOActors::Reader
-             :reader
-           elsif a.actor_class == IOActors::Writer
-             :writer
-           elsif [IOActors::Selector, IOActors::NIO4RSelector, IOActors::FFILibeventSelector].any?{ |c| a.actor_class == c }
-             :selector
-           elsif a.actor_class == PingPongActor
-             :ping_pong_actor
-           else
-             :other
-           end
-
-      stats[k][cl] ||= 0
-      stats[k][cl] += 1
-    end
-
-
-    puts %{
-PINGS: #{ PingPongStats.pings }
-PONGS: #{ PingPongStats.pongs }
-}
-
-    puts %{
-TOTAL: #{stats.map{ |k,v| v.map{ |k,v| v }.inject(0, :+) }.inject(0, :+)}
-TERMINATED: #{ stats[:terminated].map{|k,v| v}.inject(0, :+) }
- - Controller: #{ stats[:terminated][:controller] || 0 }
- - Reader: #{ stats[:terminated][:reader] || 0 }
- - Writer: #{ stats[:terminated][:writer] || 0 }
- - Selector: #{ stats[:terminated][:selector] || 0 }
- - PingPongActor: #{ stats[:terminated][:ping_pong_actor] || 0 }
- - Other:  #{ stats[:terminated][:other] || 0 }
-ALIVE: #{ stats[:alive].map{ |k,v| v }.inject(0, :+) }
- - Controller: #{ stats[:alive][:controller] || 0 }
- - Reader: #{ stats[:alive][:reader] || 0 }
- - Writer: #{ stats[:alive][:writer] || 0 }
- - Selector: #{ stats[:alive][:selector] || 0 }
- - PingPongActor: #{ stats[:alive][:ping_pong_actor] || 0 }
- - Other:  #{ stats[:alive][:other] || 0 }
-}
-
-    bev_count = 0
-    read_enabled = 0
-    write_enabled = 0
-    ObjectSpace.each_object(FFI::Libevent::BufferEvent) do |bev|
-      bev_count += 1
-      begin
-        read_enabled += 1 if bev.enabled? :read
-        write_enabled += 1 if bev.enabled? :write
-      rescue ArgumentError
-      end
+      stats[k] += 1
     end
 
     puts %{
-bufferevents: #{bev_count}
-read enabled: #{read_enabled}
-write enabled: #{write_enabled}
+FAILED AGENTS: #{stats[:failed]}
+ALIVE AGENTS: #{stats[:alive]}
 }
 
     io_count = 0
@@ -176,7 +137,7 @@ IO open: #{io_open}
 IO closed: #{io_closed}
 
 }
-  rescue Exception => e
+  rescue => e
     puts e.to_s
   ensure
     sleep 5
---
+++ examples/ping_ponger.rb
@@ -0,0 +1,69 @@
+require 'securerandom'
+
+class PingPonger
+  include IOActors::Listener
+  include Concurrent::Concern::Logging
+
+  PING = 'ping' #SecureRandom.random_bytes(1000)
+  PONG = 'PONG' #SecureRandom.random_bytes(1000)
+
+  def initialize type, generation, number, io
+    @io = io
+
+    @label = "#{type}-#{generation}-#{number}"
+
+    @selector = IOActors.default_selector
+    @received = Concurrent::Agent.new([], :error_mode => :continue)
+
+    on_read do |bytes|
+      @received.send do |received|
+        dispatch_received received.dup.push(bytes)
+      end
+    end
+
+    on_error do |e|
+      #log(Logger::ERROR, @label, e.to_s)
+      self.close
+    end
+
+    on_write do |count|
+      log(Logger::DEBUG, @label, "Wrote #{count} bytes")
+    end
+
+    @selector.add @io, self
+  end
+
+  def start!
+    @selector.write @io, PING
+  end
+
+  def die!
+    @selector.remove [@io]
+    close
+  end
+
+  def close
+    @io.close rescue nil
+  end
+
+  def dispatch_received received
+    received.join("").tap do |str|
+      case str
+      when PING
+        log(Logger::DEBUG, @label, "got PING")
+        PingPongStats.inc_pings
+        received.clear
+        @selector.write @io, PONG rescue die!
+      when PONG
+        log(Logger::DEBUG, @label, "got PONG")
+        PingPongStats.inc_pongs
+        received.clear
+        @selector.write @io, PING rescue die!
+      end
+    end
+  rescue => e
+    log(Logger::ERROR, @label + "#dispatch_received", e.to_s)
+  ensure
+    return received
+  end
+end
